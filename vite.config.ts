bde7d9ae7ce2e5f1be00176e1c81e7d4e675e203
Francois <francois.lachese@gmail.com>
2026-03-04 00:10:16 +0100

    formating

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  server: mode === 'development' ? {
-    proxy: {
-      '/status.json': {
-        target: 'https://valheim.flcloud.ovh',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  } : undefined,
-}))
+  server:
+    mode === "development"
+      ? {
+          proxy: {
+            "/status.json": {
+              target: "https://valheim.flcloud.ovh",
+              changeOrigin: true,
+              secure: true,
+            },
+          },
+        }
+      : undefined,
+}));
