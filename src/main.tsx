f8bef3c3ae9c7e22ffa8623ad2432bdf1ac3f9a4
Francois Lachese <francois.lachese@gmail.com>
2026-03-08 01:24:50 +0100

    refactor: integrate theme system into application

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
+import "./themes.css";
 
 import { LocaleProvider } from "./i18n/LocaleContext.tsx";
+import { ThemeProvider } from "./theme/ThemeContext.tsx";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
@@ -32,22 +33,24 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LocaleProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <TanStackDevtools
-          plugins={[
-            {
-              name: "TanStack Query",
-              render: <ReactQueryDevtoolsPanel />,
-            },
-            {
-              name: "TanStack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
-      </QueryClientProvider>
-    </LocaleProvider>
+    <ThemeProvider>
+      <LocaleProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <TanStackDevtools
+            plugins={[
+              {
+                name: "TanStack Query",
+                render: <ReactQueryDevtoolsPanel />,
+              },
+              {
+                name: "TanStack Router",
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+        </QueryClientProvider>
+      </LocaleProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
